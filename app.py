e2f4d90bd6f423b3e1d0c4efdca6a7d312f24f77
Mindiola <maikmj101@gmail.com>
2022-12-09 11:58:12 -0500

    Data almacen

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ ca = certifi.where()
 client = pymongo.MongoClient("mongodb://Mind:Mindiola97_@ac-lfv0xhc-shard-00-00.8jzlylr.mongodb.net:27017,ac-lfv0xhc-shard-00-01.8jzlylr.mongodb.net:27017,ac-lfv0xhc-shard-00-02.8jzlylr.mongodb.net:27017/?ssl=true&replicaSet=atlas-4m3kaw-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.test
 
-baseDatos = client["CarData"] 
+baseDatos = client["CarData"]
 print(baseDatos.list_collection_names())
 
 @app.route('/')
@@ -101,6 +101,56 @@ def updateCar(_id):
 def deleteCar(_id):
     baseDatos.carData.delete_one({"_id": ObjectId(_id)})
     return jsonify({'message' : 'Car '+ _id +' borrado correctamente'})
+
+
+
+#----------------ALMACEN-----------------------
+# Metods: POST
+# Opcion para agregar a la base de datos.
+@cross_origin
+@app.route('/alm', methods=['POST'])
+def addCarAlm():
+    id = request.json['id']
+    placa = request.json['placa']
+    marca = request.json['marca']
+    modelo = request.json['modelo']
+    kilometraje = request.json['kilometraje']
+    transmision = request.json['transmision']
+    tipo = request.json['tipo']
+    precio = request.json['precio']
+
+    if id and placa and marca and modelo and kilometraje and transmision and tipo and precio:
+        carNew = CarsData(id, placa, marca, modelo, kilometraje, transmision, tipo, precio)
+        baseDatos.almacen.insert_one(carNew.toDBCollection())
+        response = jsonify({
+            "id" : id,
+            "placa" : placa,
+            "marca" : marca,
+            "modelo" : modelo,
+            "kilometraje" : kilometraje,
+            "transmision" : transmision,
+            "tipo" : tipo,
+            "precio" : precio
+        })
+        return response
+    return notFound()
+
+# Metods: GET
+# Opcion para obtener los datos de todos los autos registrados.
+@cross_origin
+@app.route('/alm', methods=['GET'])
+def getCarsAlm():
+    cars = baseDatos.almacen.find()
+    response = json_util.dumps(cars)
+    return Response (response, mimetype='application/json')
+
+# Opcion para obtener los datos de un auto registrado por su id.
+@cross_origin
+@app.route('/alm/<id>', methods=['GET'])
+def getCarAlm(id):
+    car = baseDatos.almacen.find_one({"_id": ObjectId(id)})
+    response = json_util.dumps(car)
+    return Response (response, mimetype='application/json')
 
 
 #----------------ERROR-----------------------
